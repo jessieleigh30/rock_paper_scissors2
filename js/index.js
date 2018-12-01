c47fbd6d722c9607a26799b40cc1f6edd4cc0baf
Jessica Anderson <jessie.leigh30@gmail.com>
2018-11-30 17:10:44 -0700

    game works

--- js/index.js
+++ js/index.js
@@ -1,41 +1,77 @@
-var userChoice;
-var win = { rock:"scissors", scissors: "paper", paper:"rock"};
+var userChoice; //prompt("Welcome! Please click rock, paper or scissors.");
+//var win = { rock:"scissors", scissors: "paper", paper:"rock"};
 var choice = ["rock", "paper", "scissors"];
-var computerChoice = choice[Math.floor(Math.random()*choice.length)];
-console.log(computerChoice)
+var computerResult;
+var result;
+
+
 
 var rock = document.body.querySelector(".rock");
-  rock.addEventListener("click", function(){
-    userChoice = "rock";
-    alert('you selected rock')
-  }) 
-  var paper = document.body.querySelector(".paper");
-  rock.addEventListener("click", function(){
-    userChoice = "paper";
-    
-  }) 
-  var scissors = document.body.querySelector(".scissors");
-  rock.addEventListener("click", function(){
-    userChoice = "scissors";
-   
-    // call computer choice computerChoice()
-  }) 
+rock.addEventListener("click", function(){
+  userChoice = "rock";
+  // alert("you picked rock")
+  computerChoice();
+});
+var paper = document.body.querySelector(".paper");
+paper.addEventListener("click", function(){
+  userChoice = "paper";
+  computerChoice();
+  
+}); 
+var scissors = document.body.querySelector(".scissors");
+scissors.addEventListener("click", function(){
+  userChoice = "scissors";
+  computerChoice();
+  // call computer choice computerChoice()
+}); 
 
- var compare function(computerChoice, userChoice) 
-   if(userChoice==="rock"){
-      if(computerChoice==="scissors"){
-          return "rock wins";
-             }
-        else{
-          return "paper wins";
-    }
-   }
+function computerChoice(){
+  computerResult = choice[Math.floor(Math.random()*choice.length)];
+  console.log(computerResult, userChoice);
+  compare();
+};
+  
+function compare(){
+  switch(computerResult) {
+    case "rock":
+      if(userChoice === computerResult) {
+        result = "Tie!"
+      } else if(userChoice === "scissors"){
+        result = "You Win!"
+      } else {
+        result = "You lose."
+      }
+      break;
+    case "scissors":
+      if(userChoice === computerResult){
+        result = "Tie!"
+      } else if(userChoice === "rock"){
+        result = "You win!"
+      } else {
+        result = "You lose."
+      }
+      break;
+      case "paper":
+        if(userChoice === computerResult){
+          result = "Tie"
+        } else if(userChoice === "rock"){
+          result = "You lose."
+        } else {
+          result = "You win!"
+        }
+        break;
+      }
+      displayResult()
+  };
+
+function displayResult() {
+   var resultContainer = document.body.querySelector("#result");
+   resultContainer.innerHTML = result;
+   resultContainer.style.color = "gray";
+}
+  
+//call another function we create to display result
+
+  
   
  
-// var img = document.getElementById(img);
-
-// img.addEventListener("click", pickAnswer);
-
-//function pickAnswer(){
-  //Document.body CODE HERE = choice[Math.floor(Math.random()*choice.length)];
-//}
